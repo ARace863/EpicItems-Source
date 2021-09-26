79bc3892d5e6abf46ed24f51e2590836473d8054
ARace863 <86178679+ARace863@users.noreply.github.com>
2021-09-26 06:27:17 +0400

    Add files via upload

---
+++ src/main/java/me/arace863/epicitems/Utils/Cooldown.java
@@ -0,0 +1,27 @@
+package me.arace863.epicitems.Utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class Cooldown {
+    public static HashMap<UUID, Double> cooldowns;
+
+    public Cooldown() {
+    }
+
+    public static void setupCooldown() {
+        cooldowns = new HashMap();
+    }
+
+    public static void setCooldown(Player player, int seconds) {
+        double delay = (double)(System.currentTimeMillis() + (long)(seconds * 1000));
+        cooldowns.put(player.getUniqueId(), delay);
+    }
+
+    public static boolean checkCooldown(Player player) {
+        return !cooldowns.containsKey(player.getUniqueId()) || (Double)cooldowns.get(player.getUniqueId()) <= (double)System.currentTimeMillis();
+    }
+}
