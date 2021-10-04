72cb9425395350b72287fc6d5cfe4a880964052e
ARace863 <86178679+ARace863@users.noreply.github.com>
2021-10-04 19:15:08 +0400

    Add files via upload

---
+++ src/main/java/me/arace863/epicitems/Events/ItemAbilities/EndermanBowEvent.java
@@ -0,0 +1,80 @@
+package me.arace863.epicitems.Events.ItemAbilities;
+
+import me.arace863.epicitems.EpicItems;
+import me.arace863.epicitems.Items.ItemManager;
+import me.arace863.epicitems.Utils.Cooldown;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+
+public class EndermanBowEvent implements Listener {
+
+    FileConfiguration config;
+
+    public EndermanBowEvent(EpicItems plugin) {
+        this.config = plugin.getConfig();
+    }
+
+    @EventHandler
+    public void onShoot(EntityShootBowEvent event){
+        if (event.getEntity() instanceof Player){
+            if (event.getBow() != null && event.getBow().getItemMeta() != null && event.getBow().getItemMeta().getLore() != null
+            && event.getBow().getItemMeta().getLore().contains(ItemManager.EndermanBow.getItemMeta().getLore().get(0))){
+                if (event.getProjectile() instanceof Arrow){
+                    Arrow arrow = (Arrow) event.getProjectile();
+                    arrow.setCustomName("Enderman Bow");
+                }
+            }
+        }
+    }
+
+
+    @EventHandler
+    public void onBowShoot(ProjectileHitEvent event) {
+        if (event.getEntity() instanceof Arrow){
+            Arrow arrow = (Arrow) event.getEntity();
+            Entity entity = event.getEntity();
+                if (entity instanceof Player){
+                    Player player = (Player) entity;
+                    if (player.getInventory().getItemInMainHand().getItemMeta().hasLore()){
+                        if (player.getInventory().getItemInMainHand().getItemMeta().getLore().contains("§8EpicItem-EndermanBow")){
+                            if (Cooldown.checkCooldown(player)){
+                                Location location = event.getEntity().getLocation();
+                                event.getEntity().remove();
+                                location.setYaw(player.getLocation().getYaw());
+                                location.setPitch(player.getLocation().getPitch());
+                                player.teleport(location);
+                                player.playSound(location, Sound.ENTITY_ENDERMAN_TELEPORT, 10.0F, 1.0F);
+                                player.sendMessage(ChatColor.DARK_GRAY + "§lSwoosh!");
+                                Cooldown.setCooldown(player, this.config.getInt("EndermanBowCooldown"));
+                            }else{
+                                player.sendMessage(ChatColor.RED + "This is on cooldown for " + Math.round(((Double)Cooldown.cooldowns.get(player.getUniqueId()) - (double)System.currentTimeMillis()) / 1000.0D) + "s.");
+                            }
+                        }
+                    }
+                }
+        }
+    }
+
+    @EventHandler
+    public void onClick(PlayerInteractEvent event) {
+        if (this.config.getBoolean("ExplosiveBow", true)){
+            Player player = event.getPlayer();
+            if (this.config.getBoolean("EndermanBowQuiver", true)){
+                if (event.getItem() != null && event.getItem().getItemMeta().equals(ItemManager.ExplosiveBow.getItemMeta())) {
+                    player.getInventory().setItemInOffHand(ItemManager.Quiver);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/me/arace863/epicitems/Utils/Managers/ParticleManager.java
@@ -0,0 +1,154 @@
+package me.arace863.epicitems.Utils.Managers;
+
+import me.arace863.epicitems.EpicItems;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+
+public class ParticleManager {
+    EpicItems plugin;
+
+    public ParticleManager(EpicItems plugin) {
+        this.plugin = plugin;
+    }
+
+    public void waveParticle(Entity entity) {
+        World world = entity.getWorld();
+        new BukkitRunnable() {
+            double t = Math.PI / 4;
+            Location loc = entity.getLocation();
+
+            public void run() {
+                t = t + 0.1 * Math.PI;
+                for (double theta = 0; theta <= 2 * Math.PI; theta = theta + Math.PI / 32) {
+                    double x = t * Math.cos(theta);
+                    double y = 2 * Math.exp(-0.1 * t) * Math.sin(t) + 1.5;
+                    double z = t * Math.sin(theta);
+                    loc.add(x, y, z);
+                    world.spawnParticle(Particle.FIREWORKS_SPARK, loc, 1, 0, 0, 0, 0);
+                    loc.subtract(x, y, z);
+
+                    theta = theta + Math.PI / 64;
+
+                    x = t * Math.cos(theta);
+                    y = 2 * Math.exp(-0.1 * t) * Math.sin(t) + 1.5;
+                    z = t * Math.sin(theta);
+                    loc.add(x, y, z);
+                    world.spawnParticle(Particle.SPELL_WITCH, loc, 1, 0, 0, 0);
+                    loc.subtract(x, y, z);
+                }
+                if (t > 20) {
+                    this.cancel();
+                }
+            }
+
+        }.runTaskTimer(this.plugin, 0, 1);
+    }
+
+    public void fireSpiral(Entity entity){
+        final World world = entity.getWorld();
+        new BukkitRunnable() {
+            Location loc = entity.getLocation();
+            double t = 0;
+            double r = 1;
+
+            @Override
+            public void run() {
+                t = t + Math.PI/8;
+                double x = r*cos(t);
+                double y = t;
+                double z = r*sin(t);
+                loc.add(x, y, z);
+                world.spawnParticle(Particle.FLAME, loc, 0, 0, 0, 0, 1);
+                loc.subtract(x, y, z);
+                if (t > Math.PI*4){
+                    this.cancel();
+                }
+            }
+        }.runTaskTimer(plugin, 0, 1);
+    }
+
+    public void ashCone(final Entity entity){
+        new BukkitRunnable() {
+            double phi = 0;
+
+            @Override
+            public void run() {
+                phi += Math.PI/16;
+                double x; double y; double z;
+                Location loc = entity.getLocation();
+                World world = entity.getWorld();
+                for (double t = 0; t <= 2*Math.PI; t += Math.PI/16) {
+                    for (double i = 0; i <= 1; i += 1){
+                        x = 0.15*(2*Math.PI-t)*cos(t + phi + i*Math.PI);
+                        y = 0.5*t;
+                        z = 0.15*(2*Math.PI-t)*sin(t + phi + i*Math.PI);
+                        loc.add(x,y,z);
+                        world.spawnParticle(Particle.ASH, loc, 0, 0, 0, 0, 1);
+                        loc.subtract(x,y,z);
+                    }
+                }
+                if (phi > 10*Math.PI) {
+                    this.cancel();
+                }
+            }
+        }.runTaskTimer(plugin, 0, 2);
+    }
+
+    public void waterSphere(Entity entity){
+        final Location loc = entity.getLocation();
+        World world = entity.getWorld();
+        new BukkitRunnable() {
+            double phi = 0;
+            @Override
+            public void run() {
+                phi += Math.PI/10;
+                for (double theta = 0; theta <= 2*Math.PI; theta += Math.PI/40) {
+                    double r = 1.5;
+                    double x = r*cos(theta)*sin(phi);
+                    double y = r*cos(phi) + 1.5;
+                    double z = r*sin(theta)*sin(phi);
+                    loc.add(x,y,z);
+                    world.spawnParticle(Particle.WATER_DROP, loc, 0, 0, 0, 0, 1);
+                    loc.subtract(x,y,z);
+                }
+
+                if (phi > Math.PI){
+                    this.cancel();
+                }
+            }
+        }.runTaskTimer(plugin, 0, 1);
+    }
+
+    public void lavaSphere(Entity entity){
+        final Location loc = entity.getLocation();
+        World world = entity.getWorld();
+        new BukkitRunnable() {
+            double phi = 0;
+            @Override
+            public void run() {
+                phi += Math.PI/10;
+                for (double theta = 0; theta <= 2*Math.PI; theta += Math.PI/40) {
+                    double r = 1.5;
+                    double x = r*cos(theta)*sin(phi);
+                    double y = r*cos(phi) + 1.5;
+                    double z = r*sin(theta)*sin(phi);
+                    loc.add(x,y,z);
+                    world.spawnParticle(Particle.FLAME, loc, 0, 0, 0, 0, 1);
+                    loc.subtract(x,y,z);
+                }
+
+                if (phi > Math.PI){
+                    this.cancel();
+                }
+            }
+        }.runTaskTimer(plugin, 0, 1);
+    }
+
+    // TODO: 9/26/2021 Add FireBallRod particle here from advanced bukkit tutorials guide 
+}
